475ca97401ddd1aff9222a0ca3df05a942ad6d63
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-12 09:34:24 -0300

    create home component

--- fetch-redux/src/components/Home.jsx
+++ fetch-redux/src/components/Home.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import doFecthData from '../helpers/doFetchData';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {}, []);
+
+  if (!product) return <p>No items to show</p>;
+
+  return (
+    <>
+      <h1>{product.title}</h1>
+      <h2>{product.price}</h2>
+      <p>{product.description}</p>
+    </>
+  );
 };
 
 export default Home;
